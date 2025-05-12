7f26ef3ed57e2635e477d17b6335a4e1341e82b0
Pagliaraniz <giacomo.pagliarani.stud@ispascalcomandini.it>
2025-05-12 11:56:00 +0200

    migliorato

--- app.py
+++ app.py
@@ -2,18 +2,15 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-flashcards_db = [
-    {"materia": "Matematica", "domanda": "2+2", "risposta": "4"},
-    {"materia": "Matematica", "domanda": "5*5", "risposta": "25"},
-    {"materia": "Storia", "domanda": "Anno scoperta America", "risposta": "1492"},
-    {"materia": "Scienze", "domanda": "Formula acqua", "risposta": "H2O"}
-]
+MATERIE = ["Informatica", "Sistemi e Reti", "TPSIT", "Matematica"]
+
+flashcards_db = []
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/creazione', methods=['GET', 'POST'])
+@app.route('/crea', methods=['GET', 'POST'])
 def crea():
     if request.method == 'POST':
         nuova_flashcard = {
@@ -24,12 +21,11 @@ def crea():
         flashcards_db.append(nuova_flashcard)
         return redirect(url_for('home'))
     
-    return render_template('creazione.html')
+    return render_template('creazione.html', materie=MATERIE)
 
 @app.route('/esercitati')
 def esercitati():
-    materie = list(set(card['materia'] for card in flashcards_db))
-    return render_template('esercitazione.html', materie=materie, flashcards=flashcards_db)
+    return render_template('esercitazione.html', materie=MATERIE, flashcards=flashcards_db)
 
 if __name__ == '__main__':
     app.run(debug=True)
